12d874607cfb3a54f9a253900645f37de0edf2fb
fupeng <fupenglxy@gmail.com>
2020-10-11 15:31:40 +0800

    fix: slash export

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,6 @@ export { Event, eventCenter };
 export * from './promise';
 export { default as noop } from './noop';
 export { default as uuid } from './uuid';
+export { default as slash } from './slash';
 export { default as shallowEqual } from './shallow-equal';
 export * from './regex_constant';
-export * from './slash';
