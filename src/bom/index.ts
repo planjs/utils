ed8190646bb57eaf97bf551e852827a057475150
fupeng <fupenglxy@gmail.com>
2021-06-07 21:27:23 +0800

    chore: typings

--- src/bom/index.ts
+++ src/bom/index.ts
@@ -2,8 +2,8 @@ export { default as appendScript } from './append-script';
 export { default as executeCopy } from './execute-copy';
 export { default as windowSmoothScrolling } from './window-smooth-scrolling';
 export { default as fetch, shim as fetchShim } from './fetch';
-export {
-  default as XHRUploadFile,
+export { default as XHRUploadFile } from './xhr-upload';
+export type {
   UploadRequestMethod,
   UploadRequestHeader,
   UploadRequestOption,
