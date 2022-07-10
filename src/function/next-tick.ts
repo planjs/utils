218cc8c9af49bab1bfbd73d6dae1e791b4f81787
fupeng <fupenglxy@gmail.com>
2022-07-10 22:50:03 +0800

    fix: next-tick iife

--- src/function/next-tick.ts
+++ src/function/next-tick.ts
@@ -3,34 +3,26 @@ import { ensureCallable } from '../debug';
 
 import type { AnyFn } from '../type';
 
-function _nextTick(): (callback: Function, ...args: any[]) => void {
-  // nodejs
-  if (isFunction(typeof process === 'object' && process && process.nextTick)) {
-    return process.nextTick;
-  }
-
-  if (isFunction(typeof queueMicrotask == 'function' && queueMicrotask)) {
-    return function (cb) {
-      queueMicrotask(ensureCallable(cb));
-    };
-  }
-
-  if (isFunction(typeof setImmediate === 'function' && setImmediate)) {
-    return function (cb, ...args) {
-      setImmediate(ensureCallable<AnyFn>(cb), ...args);
-    };
-  }
-
-  return function (cb, ...args) {
-    setTimeout(ensureCallable(cb), 0, ...args);
-  };
-}
-
 /**
  * 延迟执行
  * event loop
  * @category Function
  */
-const nextTick = _nextTick();
+function nextTick(callback: Function, ...args: any[]) {
+  // nodejs
+  if (isFunction(typeof process === 'object' && process && process.nextTick)) {
+    return process.nextTick(ensureCallable<AnyFn>(callback), ...args);
+  }
+
+  if (typeof queueMicrotask == 'function') {
+    return queueMicrotask(ensureCallable<AnyFn>(callback));
+  }
+
+  if (typeof setImmediate === 'function') {
+    return setImmediate(ensureCallable<AnyFn>(callback), ...args);
+  }
+
+  return setTimeout(ensureCallable<AnyFn>(callback), 0, ...args);
+}
 
 export default nextTick;
