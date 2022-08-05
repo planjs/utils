0dbd4a14463aeac271b4184bd71227d3b226daca
fupeng <fupenglxy@gmail.com>
2022-08-05 10:35:06 +0800

    feat: add ssr window document

---
+++ src/bom/document.ts
@@ -0,0 +1,54 @@
+export const ssrDocument = {
+  body: {},
+  addEventListener() {},
+  removeEventListener() {},
+  activeElement: {
+    blur() {},
+    nodeName: '',
+  },
+  querySelector() {
+    return null;
+  },
+  querySelectorAll() {
+    return [];
+  },
+  getElementById() {
+    return null;
+  },
+  createEvent() {
+    return {
+      initEvent() {},
+    };
+  },
+  createElement() {
+    return {
+      children: [],
+      childNodes: [],
+      style: {},
+      setAttribute() {},
+      getElementsByTagName() {
+        return [];
+      },
+    };
+  },
+  createElementNS() {
+    return {};
+  },
+  importNode() {
+    return null;
+  },
+  location: {
+    hash: '',
+    host: '',
+    hostname: '',
+    href: '',
+    origin: '',
+    pathname: '',
+    protocol: '',
+    search: '',
+  },
+};
+
+export function shim() {
+  if (!global.document) global.document = ssrDocument as unknown as Document;
+}
---
+++ src/bom/window.ts
@@ -0,0 +1,62 @@
+import { ssrDocument } from './document';
+import requestAnimationFrame from './requestAnimationFrame';
+import cancelAnimationFrame from './cancelAnimationFrame';
+import isObject from '../is/is-Object';
+
+export const ssrWindow = {
+  document: ssrDocument,
+  navigator: {
+    userAgent: '',
+  },
+  location: {
+    hash: '',
+    host: '',
+    hostname: '',
+    href: '',
+    origin: '',
+    pathname: '',
+    protocol: '',
+    search: '',
+  },
+  history: {
+    replaceState() {},
+    pushState() {},
+    go() {},
+    back() {},
+  },
+  CustomEvent: function CustomEvent() {
+    return this;
+  },
+  addEventListener() {},
+  removeEventListener() {},
+  getComputedStyle() {
+    return {
+      getPropertyValue() {
+        return '';
+      },
+    };
+  },
+  Image() {},
+  Date() {},
+  screen: {},
+  setTimeout() {},
+  clearTimeout() {},
+  matchMedia() {
+    return {};
+  },
+  requestAnimationFrame: requestAnimationFrame,
+  cancelAnimationFrame: cancelAnimationFrame,
+};
+
+function extend(target: any = {}, src: any = {}) {
+  Object.keys(src).forEach((key) => {
+    if (typeof target[key] === 'undefined') target[key] = src[key];
+    else if (isObject(src[key]) && isObject(target[key]) && Object.keys(src[key])) {
+      extend(target[key], src[key]);
+    }
+  });
+}
+
+export function shim() {
+  return extend(global, ssrWindow);
+}
