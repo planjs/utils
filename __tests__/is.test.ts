03a45895379fbab0e4bbee9e3fb896cfd8d366e8
fupeng <fupenglxy@gmail.com>
2023-07-05 10:16:37 +0800

    feat: add isIterable

--- __tests__/is.test.ts
+++ __tests__/is.test.ts
@@ -1,28 +1,37 @@
-import { isNode, isBrowser, isIE, isInfinite, isDigit } from '../src/is';
+import { isNode, isBrowser, isIE, isInfinite, isDigit, isIterable } from '../src/is';
 
 describe('is', () => {
   test('is-platform', () => {
-    expect(isNode()).toBe(true);
-    expect(isBrowser()).toBe(true);
-    expect(isIE()).toBe(false);
+    expect(isNode()).toBeTruthy();
+    expect(isBrowser()).toBeTruthy();
+    expect(isIE()).toBeFalsy();
   });
 
   test('is-Infinite', () => {
-    expect(isInfinite(Infinity)).toBe(true);
-    expect(isInfinite(-Infinity)).toBe(true);
-    expect(isInfinite(1)).toBe(false);
-    expect(isInfinite(0)).toBe(false);
-    expect(isInfinite('1')).toBe(false);
+    expect(isInfinite(Infinity)).toBeTruthy();
+    expect(isInfinite(-Infinity)).toBeTruthy();
+    expect(isInfinite(1)).toBeFalsy();
+    expect(isInfinite(0)).toBeFalsy();
+    expect(isInfinite('1')).toBeFalsy();
   });
 
   test('is-Digit', () => {
-    expect(isDigit('1')).toBe(true);
-    expect(isDigit('1.2')).toBe(true);
-    expect(isDigit('-1.2')).toBe(true);
-    expect(isDigit(1.2)).toBe(true);
-    expect(isDigit(-1.2)).toBe(true);
-    expect(isDigit(Infinity)).toBe(false);
-    expect(isDigit(-Infinity)).toBe(false);
-    expect(isDigit('1.x')).toBe(false);
+    expect(isDigit('1')).toBeTruthy();
+    expect(isDigit('1.2')).toBeTruthy();
+    expect(isDigit('-1.2')).toBeTruthy();
+    expect(isDigit(1.2)).toBeTruthy();
+    expect(isDigit(-1.2)).toBeTruthy();
+    expect(isDigit(Infinity)).toBeFalsy();
+    expect(isDigit(-Infinity)).toBeFalsy();
+    expect(isDigit('1.x')).toBeFalsy();
+  });
+
+  test('isIterable', () => {
+    function* makeIterator() {
+      yield 1;
+      yield 2;
+    }
+    const it = makeIterator();
+    expect(isIterable(it)).toBeTruthy();
   });
 });
